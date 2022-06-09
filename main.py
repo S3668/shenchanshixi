068ce30b00c8bd5eb4c238d138c8c46883f3a7db
S.l <406047187@qq.com>
2022-06-09 19:20:39 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from window import register
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    MainWindow = QMainWindow()
+    ui = register.Ui_Form()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec())
